dc6311f8e8556c56ea3c91f0fe6265a807d360bf
Egaster <egorsmirnov160602@gmail.com>
2024-04-04 14:24:34 +0300

    сверстал первую страницу, добавил 2 пустые страницы с переходом

--- client/flutter_application_1/lib/main.dart
+++ client/flutter_application_1/lib/main.dart
@@ -4,17 +4,103 @@ import 'src/app.dart';
 import 'src/settings/settings_controller.dart';
 import 'src/settings/settings_service.dart';
 
-void main() async {
-  // Set up the SettingsController, which will glue user settings to multiple
-  // Flutter Widgets.
-  final settingsController = SettingsController(SettingsService());
 
-  // Load the user's preferred theme while the splash screen is displayed.
-  // This prevents a sudden theme change when the app is first displayed.
-  await settingsController.loadSettings();
+void main() {
+  runApp(MyApp());
+}
 
-  // Run the app and pass in the SettingsController. The app listens to the
-  // SettingsController for changes, then passes it further down to the
-  // SettingsView.
-  runApp(MyApp(settingsController: settingsController));
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(
+          child: Text('Вход',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+            ),
+          ),
+        ) 
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                Column(
+                  children: <Widget>[
+                    IconButton(
+                      icon:  Image.asset('assets/images/telegram.png', width: 80, height: 80),
+                      onPressed: () {
+                        Navigator.push(context, MaterialPageRoute(builder: (context) => PageOne()));
+                      },
+                    ),
+                    const Text('Telegram',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ]
+                ),
+                Column(
+                  children: <Widget>[
+                    IconButton(
+                      icon:  Image.asset('assets/images/VK.png', width: 90, height: 90),
+                      onPressed: () {
+                        Navigator.push(context, MaterialPageRoute(builder: (context) => PageTwo()));
+                      },
+                    ),
+                    const Text('VK',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ]
+                ),
+              ]
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class PageOne extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Page One'),
+      ),
+      body: Center(
+        child: Text('This is Page One'),
+      ),
+    );
+  }
+}
+
+class PageTwo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Page Two'),
+      ),
+      body: Center(
+        child: Text('This is Page Two'),
+      ),
+    );
+  }
 }
