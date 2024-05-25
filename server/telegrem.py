7e4c4cec719bee59210ec1b83f0d3ae17e73a0fb
makardom <domanov551@gmail.com>
2024-05-25 15:53:08 +0300

    qwerrt

--- server/telegrem.py
+++ server/telegrem.py
@@ -6,15 +6,27 @@ app = Flask(__name__)
 # app.config['SQLALCHEMY_DATABASE_URI'] = ''
 
 base = {}
+base2 = {}
 @app.route('/tg/numbers')
 def all_base_phones():
-    return base;
+    return base2;
 
 @app.route('/tg', methods=['GET'])
 def phone_number():
     base['Number'] = str(request.args['Number'])
-    #base['Number2'] = str(request.args['Number2'])
+    base['Number2'] = str(request.args['Number2'])
     return jsonify (base)
+
+@app.route('/tg/signup', methods=['GET'])
+def sign_up():
+    name = str(request.args['Acc'])
+    Number = str(request.args['Number'])
+    Password = str(request.args['Password'])
+    inner_dict ={}
+    inner_dict[Number] = Password
+    base2[name] = inner_dict
+    return jsonify (base2)
+
 
 if __name__ == '__main__':
     app.run()
